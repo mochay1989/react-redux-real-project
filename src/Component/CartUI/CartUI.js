72b03b592d51303421c9278a587901df089bcb4c
Han-Moe-Htet <hapo23lert@gmail.com>
2021-05-27 11:30:10 +0630

    .

--- src/Component/CartUI/CartUI.js
+++ src/Component/CartUI/CartUI.js
@@ -1,37 +1,51 @@
-import React, { Component } from "react";
-import "./CartUI.css";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import './CartUI.css';
+import { connect } from 'react-redux';
+import { currentIDid } from '../../redux/action/productAction';
 
 const mapStateToProps = (state) => {
   return {
     cart: state.cartData,
+    currentid: state.currentid,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setCurrentId: (id) => dispatch(currentIDid(id)),
   };
 };
 
 export class CartUI extends Component {
+  changeCurrentId = (id) => {
+    this.props.setCurrentId(id);
+  };
+
   render() {
     return (
       <div>
         <div>
-          {this.props.cart.map((cart,id) => {
+          {this.props.cart.map((cart) => {
             return (
-              <div className="cartUI" style={
-                this.props.currentid === id
-                  ? { color: "blue", backgroundColor: "yellow" }
-                  : { color: "black" }}>
-                <div
-               
-              
-                >
+              <div
+                className="cartUI"
+                style={
+                  this.props.currentid === cart.id
+                    ? { color: 'blue', backgroundColor: 'yellow' }
+                    : { color: 'black' }
+                }
+                onClick={() => this.changeCurrentId(cart.id)}
+              >
+                <div>
                   <span>{cart.title}</span> <br></br>
                   <span>
-                    at {cart.quantity} at ${cart.price} with {cart.disc} discount
+                    at {cart.quantity} at ${cart.price} with {cart.disc}{' '}
+                    discount
                   </span>
                 </div>
 
                 <div>
-                 
-                  <span>${cart.quantity*cart.price}</span>
+                  <span>${cart.quantity * cart.price}</span>
                 </div>
               </div>
             );
@@ -42,4 +56,4 @@ export class CartUI extends Component {
   }
 }
 
-export default connect(mapStateToProps)(CartUI);
+export default connect(mapStateToProps, mapDispatchToProps)(CartUI);
